2c9931afaeda4ea1d17808e7289dadda01f6fb88
Blaine Lockard <blainelockard@gmail.com>
2026-02-27 20:36:09 -0600

    Change camera controls

--- camera_object.gd
+++ camera_object.gd
@@ -5,26 +5,16 @@ class_name CameraObject
 
 @onready var camera: Camera3D = $Camera3D
 
+
 var isActive: bool = false
 
 
-func _ready() -> void:
-	pass
-
-
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept") and isActive:
-		toNext()
-
-
 func switchTo():
-	print("hello I am: " + name)
-	print("My next is: " + next.name + "\n")
 	isActive = true
 	camera.make_current()
 
 
 func toNext():
-	if next:
+	if next and isActive:
 		isActive = false
 		next.switchTo()
--- main.gd
+++ main.gd
@@ -1,6 +1,12 @@
 extends Node3D
 
-@export var startingCam: CameraObject
+@export var currentCam: CameraObject
 
 func _ready() -> void:
-	startingCam.switchTo()
+	currentCam.switchTo()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
+			currentCam.toNext()
+			currentCam = currentCam.next
